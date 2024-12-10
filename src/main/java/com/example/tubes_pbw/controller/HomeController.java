b761bdbf0c03cca07d8fc8406d9ae13516bd10c8
Josua <josua312@gmail.com>
2024-12-10 23:57:45 +0700

    add logout

--- src/main/java/com/example/tubes_pbw/controller/HomeController.java
+++ src/main/java/com/example/tubes_pbw/controller/HomeController.java
@@ -8,14 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.example.tubes_pbw.model.artis.Artis;
 import com.example.tubes_pbw.model.artis.ArtisService;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
     @Autowired
     ArtisService artisService;
     @GetMapping("/")
-    public String showHome(Model model){
+    public String showHome(Model model, HttpSession session){
         Iterable<Artis> listArtis = artisService.findByFilterNamaArtis("");
         model.addAttribute("listArtis", listArtis);
+        if(session.getAttribute("username") == null){
+            model.addAttribute("isUserLoggedIn", false);
+        }
+        else{
+            model.addAttribute("isUserLoggedIn", true);
+        }
         return "homePage";
     }
 
